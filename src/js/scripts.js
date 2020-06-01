d0f463201b9e3d41d30a5da158e0309a57aee9b8
alexlenk <alexlenk.ru@gmail.com>
2020-06-01 23:42:15 +0300

    Fixed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -38,19 +38,21 @@ $(document).ready(function () {
 /*
 
 /* BEGIN запрет копирования с сайта */
+/*
 document.ondragstart = noselect;
 // запрет на перетаскивание
 document.onselectstart = noselect;
 // запрет на выделение элементов страницы
 document.oncontextmenu = noselect;
+*/
 
 // запрет на выведение контекстного меню
-function noselect() {
+/*function noselect() {
     return false;
-}
+}*/
 
 //запретить на сайте нажатие CTRL+SHIFT+I и F12
-document.onkeydown = function (e) {
+/*document.onkeydown = function (e) {
     if (event.keyCode == 123) {
         return false;
     }
@@ -63,5 +65,5 @@ document.onkeydown = function (e) {
     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
         return false;
     }
-};
+};*/
 /* END */
